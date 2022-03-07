6de54fd9bc44c070cf28d941748cc7e4a3e1b98d
Diego Norel Vega Mejia <diego.vega@unas.edu.pe>
2022-03-06 23:42:57 -0500

    Comenzando con el desafío

---
+++ resources/views/api.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+ <head>
+  <meta charset = "windows-1252">
+  <meta name = "viewport" content = "width=device-width, initial-scale=1">
+  <link rel = "stylesheet" href = "../css/app.css">
+ </head>
+ <body>
+  <div class="bg-ini">
+   <img class="bg-img" src="../images/png_index.png" alt=" API " title=" API ">
+  </div>
+ </body>
+</html>
